1e753cff338adf42ff5194b3f92d8c972a87df19
Lyubomir Ganev <luboganev@gmail.com>
2015-06-20 23:03:40 +0200

    Awesome design

--- Handdrawn/mobile/src/main/java/com/luboganev/handdrawn/MainActivity.java
+++ Handdrawn/mobile/src/main/java/com/luboganev/handdrawn/MainActivity.java
@@ -3,7 +3,8 @@ package com.luboganev.handdrawn;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar;
 
@@ -17,15 +18,49 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity implements RangeSeekBar.OnRangeSeekBarChangeListener {
     @InjectView(R.id.drawingView) DrawingView drawingView;
     @InjectView(R.id.rangebar) RangeSeekBar<Integer> rangeBar;
-    @InjectView(R.id.action_clear) ImageButton clearImageButton;
-    @InjectView(R.id.action_set_draw_mode) ImageButton drawImageButton;
-    @InjectView(R.id.action_set_present_mode) ImageButton presentImageButton;
+    @InjectView(R.id.action_clear) ImageView clearImageView;
+    @InjectView(R.id.action_set_draw_mode) ImageView drawImageView;
+    @InjectView(R.id.action_set_present_mode) ImageView presentImageView;
+    @InjectView(R.id.action_info) ImageView showInfoImageView;
+    @InjectView(R.id.logo) ImageView logoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+
+        View.OnLongClickListener listener = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int messageResourceId = -1;
+                switch (v.getId()) {
+                    case R.id.action_info:
+                        messageResourceId = R.string.hint_toast_info;
+                        break;
+                    case R.id.action_clear:
+                        messageResourceId = R.string.hint_toast_clear;
+                        break;
+                    case R.id.action_set_present_mode:
+                        messageResourceId = R.string.hint_toast_present_mode;
+                        break;
+                    case R.id.action_set_draw_mode:
+                        messageResourceId = R.string.hint_toast_draw_mode;
+                        break;
+                }
+                if (messageResourceId >= 0) {
+                    Toast.makeText(getApplicationContext(), messageResourceId, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        };
+
+        clearImageView.setOnLongClickListener(listener);
+        drawImageView.setOnLongClickListener(listener);
+        presentImageView.setOnLongClickListener(listener);
+        showInfoImageView.setOnLongClickListener(listener);
 
         rangeBar.setOnRangeSeekBarChangeListener(this);
         rangeBar.setNotifyWhileDragging(true);
@@ -35,14 +70,28 @@ public class MainActivity extends AppCompatActivity implements RangeSeekBar.OnRa
 
     private void updateControlPanelButtons() {
         if (drawingView.getMode() == DrawingView.MODE_DRAW) {
-            clearImageButton.setVisibility(View.VISIBLE);
-            drawImageButton.setVisibility(View.GONE);
-            presentImageButton.setVisibility(View.VISIBLE);
+            clearImageView.setVisibility(View.VISIBLE);
+            drawImageView.setVisibility(View.GONE);
+            presentImageView.setVisibility(View.VISIBLE);
         } else {
-            clearImageButton.setVisibility(View.GONE);
-            drawImageButton.setVisibility(View.VISIBLE);
-            presentImageButton.setVisibility(View.GONE);
+            clearImageView.setVisibility(View.GONE);
+            drawImageView.setVisibility(View.VISIBLE);
+            presentImageView.setVisibility(View.GONE);
         }
+    }
+
+    private Runnable mHideTutorialRunnable = new Runnable() {
+        @Override
+        public void run() {
+            drawingView.setBackground(null);
+        }
+    };
+
+    @OnClick({R.id.action_info, R.id.logo})
+    void onActionInfo() {
+        drawingView.setBackgroundResource(R.drawable.tutorial_background);
+        drawingView.removeCallbacks(mHideTutorialRunnable);
+        drawingView.postDelayed(mHideTutorialRunnable, 3000);
     }
 
     @OnClick(R.id.action_clear)
@@ -52,13 +101,13 @@ public class MainActivity extends AppCompatActivity implements RangeSeekBar.OnRa
         } else {
             drawingView.clearCanvas();
         }
-        rangeBar.setVisibility(View.GONE);
     }
 
     @OnClick(R.id.action_set_draw_mode)
     void onActionSetDrawMode() {
         drawingView.setMode(DrawingView.MODE_DRAW);
-        rangeBar.setVisibility(View.GONE);
+        rangeBar.setVisibility(View.INVISIBLE);
+        logoImageView.setVisibility(View.VISIBLE);
         updateControlPanelButtons();
     }
 
@@ -68,6 +117,7 @@ public class MainActivity extends AppCompatActivity implements RangeSeekBar.OnRa
         drawingView.setMode(DrawingView.MODE_PRESENT);
         updateRangeBar(points.size() - 1);
         rangeBar.setVisibility(View.VISIBLE);
+        logoImageView.setVisibility(View.INVISIBLE);
         drawingView.setTimedPoints(points);
         updateControlPanelButtons();
     }
