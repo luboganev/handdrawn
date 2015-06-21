9b6097e71dd4d03cca3cb3dc930e16de7d779923
Lyubomir Ganev <luboganev@gmail.com>
2015-06-21 10:13:31 +0200

    One does not simply have launcher icons

---
+++ Handdrawn/shared/src/main/java/com/luboganev/handdrawn/shared/ClientPaths.java
@@ -0,0 +1,6 @@
+package com.luboganev.handdrawn.shared;
+
+public class ClientPaths {
+    public static final String START_MEASUREMENT = "/start";
+    public static final String STOP_MEASUREMENT = "/stop";
+}
